ffd87fe6f50db89ab536536261b78ef15d25b91d
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 21:35:45 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoStringParts/SHParts.cs
+++ SunamoStringParts/SHParts.cs
@@ -1,7 +1,5 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-
 namespace SunamoStringParts;
+
 public class SHParts
 {
     /// <summary>
